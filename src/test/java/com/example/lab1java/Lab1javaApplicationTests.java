23113dcb535f64dd8f40df82c365f9a7d506a8ac
Sabit <sz4205600@gmail.com>
2024-09-09 14:06:05 +0500

    1

---
+++ src/test/java/com/example/lab1java/Lab1javaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab1java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab1javaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
